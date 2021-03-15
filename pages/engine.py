79ac217860b3bf7270c2b48404c3e0a4f88956a4
Grishin Vadim <43302420+KajimaSoys@users.noreply.github.com>
2021-03-15 16:06:40 +0300

    v.1.2

--- pages/engine.py
+++ pages/engine.py
@@ -1,14 +1,33 @@
 import time
+import enchant
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def errorCheck(querystr):
+    dictionary = enchant.Dict("ru_RU")
+    responsedef=[]
+    if dictionary.check(querystr):
+        responsedef.append('По запросу ')
+        responsedef.append(querystr)
+        responsedef.append(' найдены следующие товары:')
+        return(responsedef)
+    else:
+        querytemp = dictionary.suggest(querystr)
+        print(querytemp)
+        responsedef.append("Возможно, Вы искали ")
+        responsedef.append(querytemp)
+        responsedef.append("?")
+        return(responsedef)
+
 
 def driverwork(querystr):
     page = 1
+
     options = webdriver.ChromeOptions()
     #options.add_argument('headless')
+    #options.add_argument('--proxy-server=%s' % proxyBuilder())
     driver = webdriver.Chrome(options=options)
     datalist = parsing(querystr, driver, page)
     driver.quit()
@@ -18,11 +37,11 @@ def driverwork(querystr):
 def parsing(querystr, driver, page):
     link = 'https://www.ozon.ru/search/?from_global=true&text=' + querystr + '&page=' + str(page)
     driver.get(link)
-    #time.sleep(20)
     datalist = []
-    div = WebDriverWait(driver, 60).until(
+    div = WebDriverWait(driver, 120).until(
         EC.presence_of_element_located((By.CLASS_NAME, "a0c6.a0c9.a0c8"))
     )
+    time.sleep(2)
     div = driver.find_elements_by_class_name('a0c6.a0c9.a0c8')
     for item in div:
         data = []
@@ -30,27 +49,22 @@ def parsing(querystr, driver, page):
         name = item.find_element_by_class_name('a2g0.tile-hover-target').text
         # noinspection PyBroadException
 
-        if item.find_element_by_class_name('b5v6.b5v7'):
-            price=item.find_element_by_class_name('b5v6.b5v7').text
-        elif item.find_element_by_class_name('b5v6.b5v7.c4v8'):
-            price = item.find_element_by_class_name('b5v6.b5v7.c4v8').text
-        elif item.find_element_by_class_name('b5v6.c8c6.c4v8'):
-            price = item.find_element_by_class_name('b5v6.c8c6.c4v8').text
         try:
-            price = price.replace('\u2009', ' ')
-            price = price.replace('&thinsp;', ' ')
+            price = item.find_element_by_class_name('b5v6.b5v7.c4v8').text
         except Exception:
-            print('Возникли проблемы c Price')
-
-        #try:
-        #    price = item.find_element_by_class_name('b5v6.b5v7.c4v8').text
-        #except Exception:
-        #    try:
-        #        price = item.find_element_by_class_name('b5v6.c8c6.c4v8').text
-        #    except Exception:
-        #        pass
-        #finally:
-
+            try:
+                price = item.find_element_by_class_name('b5v6.b5v7').text
+            except Exception:
+                try:
+                    price = item.find_element_by_class_name('b5v6.c8c6.c4v8').text
+                except Exception:
+                    price = item.find_element_by_class_name('b5v6.c8c6').text
+        finally:
+            try:
+                price = price.replace('\u2009', ' ')
+                price = price.replace('&thinsp;', ' ')
+            except Exception:
+                print('Тут проблема с Price у элемента' + name)
 
         url = item.find_element_by_class_name('a0v2.tile-hover-target').get_attribute("href")
         image = item.find_element_by_tag_name("img").get_attribute("src")
@@ -60,12 +74,8 @@ def parsing(querystr, driver, page):
             try:
                 provider = subitem.find_elements_by_tag_name('font')[1].text
             except Exception:
-                pass
+                print('Тут проблема с Provider у элемента' + name)
+                provider = 'Отсутствует'
         data.extend([name, price, provider, url, image])
         datalist.append(data)
     return datalist
-
-#querystr=input()
-#result=driverwork(querystr)
-#print(result)
-# driverwork('ночник')
--- pages/views.py
+++ pages/views.py
@@ -4,10 +4,19 @@ from . import engine
 
 def my_view(request):
     Product.objects.all().delete()
+    error = ""
+    first = ""
+    link = ""
+    last = ""
+    text = ""
     if 'search' in request.GET and request.GET['search']:
         search = request.GET['search']
-        query=engine.driverwork(search)
-        #print(query)
+        query = engine.driverwork(search)
+        error = engine.errorCheck(search)
+        link = 'http://127.0.0.1:8000/?search='+search
+        first = error[0]
+        text = error[1]
+        last = error[2]
         for item in query:
             try:
                 Product.objects.create(title=item[0], price=item[1], provider=item[2], url=item[3], img=item[4])
@@ -17,6 +26,11 @@ def my_view(request):
         request,
         'pages/index.html',
         {
-            'elements': Product.objects.all()
+            'elements': Product.objects.all(),
+            'exception': error,
+            'first': first,
+            'text': text,
+            'last': last,
+            'link': link
         }
     )
